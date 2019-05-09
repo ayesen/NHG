a3914e006b5f1ab5035e9f03498c49f247b6b40d
Cooper-Yang <Ty1037@nyu.com>
2019-05-09 13:43:42 -0400

    dddd

---
+++ Assets/Scripts/DoorKeyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKeyScript : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //player.GetComponent<PlayerMove>().promptMSG = true;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                
+            }
+            
+        }
+    }
+}
