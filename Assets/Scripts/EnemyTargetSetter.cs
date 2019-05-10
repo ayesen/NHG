5690687a84498a890040e1c783e4f02a2e5e0193
PAPAPAPAYA <jy2636@nyu.edu>
2019-05-09 23:48:00 -0400

    almost done?

--- Assets/Scripts/EnemyTargetSetter.cs
+++ Assets/Scripts/EnemyTargetSetter.cs
@@ -19,7 +19,7 @@ public class EnemyTargetSetter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        activated = false;
     }
 
     // Update is called once per frame
