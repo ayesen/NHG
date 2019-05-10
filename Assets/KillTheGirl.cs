256665f66d955e2cdca0bef4d9ce7b0347637f7b
wenyi xu <ixvory@MacBook-Pro-25.fios-router.home>
2019-05-10 05:26:58 -0400

    hug you

---
+++ Assets/KillTheGirl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillTheGirl : MonoBehaviour
+{
+    public Animator monsterAnimator;
+    public GameObject girl;
+
+    Vector3 girlpos;
+    bool isPushed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPushed == true)
+        {
+            girl.transform.position = girlpos;
+        }
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            isPushed = true;
+            girlpos = girl.transform.position;
+
+            monsterAnimator.SetBool("Grab", true);
+  
+            collision.GetComponent<PlayerMove>().enabled = false;
+
+            Debug.Log("hehe");
+
+        }
+    }
+
+
+}
